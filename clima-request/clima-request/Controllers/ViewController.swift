41221bda168cfcabad88b32bb78f2f4097ad2e35
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-11-24 16:18:39 -0300

    Busca de cidades brasileiras e retorno de suas temperaturas implementado

--- clima-request/clima-request/Controllers/ViewController.swift
+++ clima-request/clima-request/Controllers/ViewController.swift
@@ -9,17 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var grausLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
     var weather: Weather?
+    var city: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        requestWeather()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func searchButtonClick(_ sender: UIButton) {
+        guard searchTextField.text != nil && searchTextField.text != "" else {return}
+        
+        city = ""
+        
+        for letter in searchTextField.text! {
+            if letter == " " {
+                city.append("%20")
+            } else {
+                city.append(letter)
+            }
+        }
+        
+        requestWeather()
+    }
+    
     func requestWeather () {
-        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Porto%20Alegre,br&units=metric&appid=aeefba332b49db396d425480b21571b2")
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city),br&units=metric&appid=aeefba332b49db396d425480b21571b2")
         
         guard url != nil else {return}
         
@@ -38,6 +61,8 @@ class ViewController: UIViewController {
                 
                 DispatchQueue.main.async {
                     print (self.weather!)
+                    self.grausLabel.text = String(self.weather!.main["temp"]!)
+                    self.cityLabel.text = (self.weather!.name)
                 }
             } catch let error {
                 print(error)
